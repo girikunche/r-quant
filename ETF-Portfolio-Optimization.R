0ad0f401f0089294b2753d659268a67f00c1377f
girikunche <kgbonnet@gmail.com>
2020-04-28 01:01:41 -0400

    Update ETF-Portfolio-Optimization.R

--- ETF-Portfolio-Optimization.R
+++ ETF-Portfolio-Optimization.R
@@ -9,10 +9,11 @@ require(ROI.plugin.quadprog)
 #Start Date
 dt<-"2010-04-01"
 #End Date
-tilldate<-"2020-03-01"
+tilldate<-"2020-04-01"
 
 #List of Stocks or ETF's for Analysis
 tickers<-c("VEA","EEM","VWO","TIP","SHV","IGOV","GSG","DJP","VNQ","EFA")
+treasury<-("^IRX")
 
 #Assign Weights
 weights<-c("0.1","0.1","0.1","0.1","0.1","0.1","0.1","0.1","0.1","0.1")
@@ -27,6 +28,9 @@ for (ticker in tickers){
   portfolioPrices<-cbind(portfolioPrices,getSymbols.yahoo(ticker,from=dt,to=tilldate,periodicity='monthly',auto.assign=FALSE)[,6])
 }
 
+#Get data from Yahoo Finance, for IRX
+irx<-getSymbols.yahoo(treasury,from=dt,to=tilldate,periodicity='monthly',auto.assign=FALSE)[,6]/1190
+
 #check if there is any missing data
 colSums(is.na(portfolioPrices))
 #if output is zero for each ticker,  that means no missing data
@@ -34,8 +38,6 @@ colSums(is.na(portfolioPrices))
 #ROC funtion calculates daily changes for individual columns
 
 portfolioReturns<-na.omit(Return.calculate(portfolioPrices))
-
-xy<-Return.portfolio(portfolioReturns,weights=NULL,geometric = TRUE)
 
 #create a benchmark to compare tickers. Compare with S&P 500 - ^GSPC
 benchmarkPrices<-getSymbols.yahoo('^GSPC',from=dt,to=tilldate,periodicity='monthly',auto.assign=FALSE)[,6]
@@ -45,67 +47,5 @@ colSums(is.na(benchmarkPrices))
 #if output is zero for each ticker,  that means no missing data
 #calculate Returns for S&P 500. Remove NA's using na.omit
 benchmarkReturns<-na.omit(Return.calculate(benchmarkPrices))
-
-#Basic Stats for each ticker
-ReturnStats<-table.Stats(portfolioReturns)
-
-
-#Calculate Beta
-#Risk free rate is 0.035 and there are 118 trading months
-CAPM.beta(xy,benchmarkReturns,0.01)
-
-#Calculate Jensen's Alpha
-CAPM.jensenAlpha(portfolioReturns,benchmarkReturns,0.01)
-
-#Sharpe Ratio
-SharpeRatio(portfolioReturns,0.01)
-
-#Annualized Returns
-table.AnnualizedReturns(portfolioReturns)
-
-#Calendar Returns
-table.CalendarReturns(portfolioReturns)
-
-
-
-
-
-
-
-
-
-
-
-#Assign names of the securities to fund.names
-fund.names<-tickers
-
-#Create a portfolio Object
-pspec<-portfolio.spec(assets = fund.names)
-print.default(pspec)
-
-#Add constraint to the Portfolio Object
-
-#Use add.contraint
-#Sum of Weight Constraint
-
-pspec<-add.constraint(portfolio=pspec, type="weight_sum",min_sum=1,max_sum=1)
-
-#Add Box constraint
-pspec<-add.constraint(portfolio=pspec, type="box",min=0.0,max=1.0)
-
-#Add objective
-#Add portfolio return objective
-pspec <-add.objective(portfolio=pspec,type='return',name='mean')
-pspec<- add.objective(portfolio=pspec,type="risk",name="var")
-
-
-
-#Solver
-
-opt_maxret <- optimize.portfolio(R=portfolioReturns, portfolio=pspec,optimize_method="ROI",trace=TRUE)
-print(opt_maxret)
-
-opt_minvar <- optimize.portfolio(R=portfolioReturns, portfolio=pspec,optimize_method="ROI",trace=TRUE)
-print(opt_minvar)
 
 
